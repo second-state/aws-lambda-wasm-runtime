edaa7efc9afdda7de40e70b9d9c8d6e042376436
robnanarivo <robby.xi.qiu@gmail.com>
2021-08-05 19:59:36 +0800

    Install tensorflow

--- api/pre.sh
+++ api/pre.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # get wasmedge & wasmedgec
-curl -sSfL https://github.com/WasmEdge/WasmEdge/releases/download/0.8.2-rc.2/WasmEdge-0.8.2-rc.2-manylinux2014_x86_64.tar.gz -o ./WasmEdge.tar.gz
-tar --strip-components 2 -xzvf WasmEdge.tar.gz WasmEdge-0.8.2-rc.2-Linux/bin
-rm WasmEdge.tar.gz
+curl -L https://github.com/second-state/WasmEdge-tensorflow-tools/releases/download/0.8.2-rc2/WasmEdge-tensorflow-tools-0.8.2-rc2-manylinux2014_x86_64.tar.gz -o ./WasmEdge-tensorflow-tools-0.8.2-rc2-manylinux2014_x86_64.tar.gz
+tar xzvf WasmEdge-tensorflow-tools-0.8.2-rc2-manylinux2014_x86_64.tar.gz wasmedge-tensorflow-lite
+tar xzvf WasmEdge-tensorflow-tools-0.8.2-rc2-manylinux2014_x86_64.tar.gz wasmedgec-tensorflow
+rm WasmEdge-tensorflow-tools-0.8.2-rc2-manylinux2014_x86_64.tar.gz
+
+curl -L https://github.com/second-state/WasmEdge-tensorflow-deps/releases/download/0.8.0/WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz -o ./WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
+tar xzvf WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
+rm WasmEdge-tensorflow-deps-TFLite-0.8.0-manylinux2014_x86_64.tar.gz
 
 # compile all .wasm to .so
 for file in *.wasm; do
     [ -f "$file" ] || continue
-    ./wasmedgec --generic-binary "$file" "${file/.wasm/.so}"
+    ./wasmedgec-tensorflow --generic-binary "$file" "${file/.wasm/.so}"
     rm "$file"
 done
 
 # clean
-rm wasmedgec
-
-ls -l
+rm wasmedgec-tensorflow
